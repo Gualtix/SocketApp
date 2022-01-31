06eda8008b56f3eb19c2021b91a534fdf3b982bb
Gualtix <walkter@gmail.com>
2022-01-31 14:49:28 -0600

    End

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SocketApp
+## Usage
+- Go to target and run: java -jar SocketApp.jar
+- This will up the Server and send an Object through Client.
+- Server Runs on a different thread but on the same jar.
+- Binary File is stored at: C:\files\Vehicle.bin
--- src/main/java/com/mycompany/socketapp/main.java
+++ src/main/java/com/mycompany/socketapp/main.java
@@ -1,26 +1,77 @@
 package com.mycompany.socketapp;
 
 import Server.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
+
 import Client.Client;
 import Model.Vehicle;
 
 public class main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
 
         Server ser = new Server(8085);
         Thread t = new Thread(ser);
         t.start(); 
+        TimeUnit.SECONDS.sleep(1);
 
 
-        Vehicle Bike = new Vehicle();
-        Bike.Category = "Motorcycle";
-        Bike.Color = "RED";
-        Bike.Weight = 575;
-        Bike.Brand = "DUCATI";
+        // Enter data using BufferReader
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+ 
+        System.out.println("");
+        System.out.println("");
+        System.out.println("This App will create a Vehicle Object and send it to the a Server via Socket");
+        System.out.println("When the Server receives the Vehicle Object, it will execute the Vehicle Object's methods like:");
+        System.out.println("- StartEngine()");
+        System.out.println("- RaceAndGetStats()");
+        System.out.println("- getLastTime()");
+        System.out.println("- getLastDistance()");
+        System.out.println("- ShowDemostration()");
+        System.out.println("- StopEngine()");
+        System.out.println("");
 
-        Client cli = new Client("localhost", 8085, Bike);
+        System.out.println("Enter Vehicle Category (Car, Motorcycle, Truck, Bicycle)");
+        String Category = reader.readLine();
+        System.out.println("");
+
+        System.out.println("Enter Vehicle Color");
+        String Color = reader.readLine();
+        System.out.println("");
+
+        System.out.println("Enter Vehicle Weight (Kg)");
+        String tmp = reader.readLine();
+        
+        Double Weight = 0.0;
+        try {
+            Weight = Double.parseDouble(tmp);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid Weight");
+            System.out.println("Default Weight: 0.0 Kg");
+        }
+        
+        System.out.println("");
+
+        System.out.println("Enter Vehicle Brand");
+        String Brand = reader.readLine();
+        System.out.println("");
+
+        Vehicle Vh   = new Vehicle();
+        Vh.Category  = Category;
+        Vh.Color     = Color;
+        Vh.Weight    = Weight;
+        Vh.Brand     = Brand;
+
+        System.out.println("");
+        System.out.println("Binary File Stored at: C:\\files\\Vehicle.bin");
+        
+
+        Client cli = new Client("localhost", 8085, Vh);
 
     }
     
